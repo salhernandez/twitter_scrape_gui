a81f6d7e5a5eac7726a51674dc218cd639546747
Salvador <hernandezg.sal@gmail.com>
2017-03-17 01:27:30 -0700

    added shell script that install all the required libraries

---
+++ requirements.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get install python-numpy
+sudo apt-get python-scipy
+sudo apt-get python-matplotlib
+sudo apt-get install python-beautifulsoup
+sudo apt-get install python-imaging-tk
+sudo pip install pydot
+sudo pip install networkx
